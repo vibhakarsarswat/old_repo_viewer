0c922dae575dd6be37fb7a70dd5f793896ba5e36
Vibhakar Sarswat <vibhakar.sarswat@productmadness.com>
2022-02-28 01:56:15 +0100

    Added a new class 'GithubAuthenticator'

---
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -0,0 +1,6 @@
+import 'package:oauth2/oauth2.dart';
+
+class GithubAuthenticator {
+  // Once User is Authenticated, below method will be used to retrieve the 'Access Token'
+  Future<Credentials> getSignedInCredentials() async {}
+}
